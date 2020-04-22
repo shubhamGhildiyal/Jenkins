2e47da742b52ef7a849d855b641391fa26cdee2e
shubham <shubhamghildiyal49@gmail.com>
2020-04-22 18:22:08 +0530

    Change in main

---
+++ Hello_project/src/shubham/ghildiyal1/Main.java
@@ -0,0 +1,9 @@
+package shubham.ghildiyal1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world ji ha hahhahahaahhaha");
+    }
+}
